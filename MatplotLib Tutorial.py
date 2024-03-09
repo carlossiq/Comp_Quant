622edd0881927eb53c8455483675652842449c2d
carlossiq <31148477+carlossiq@users.noreply.github.com>
2024-03-09 11:44:56 -0300

    09/03/24

---
+++ MatplotLib Tutorial.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ax é a região onde será plotado o gráfico
+
+# figura com um ax: fig, ax = plt.subplot()
+fig = plt.subplots()
+
+# 2 axes, um em cima outro embaixo
+fig, axs = plt.subplots(2, 1)
+
+# 1 a esquerda e 2 a direita (cima e baixo)
+fig, axs = plt.subplot_mosaic([["left", "right_top"], ["left", "right_bottom"]])
+
+figure, axes = plt.subplots()
+# circulo com centro (0.6,0.6) e raio 0.3, sem preenchimento
+Drawing_uncolored_circle = plt.Circle((0.6, 0.6), 0.3, fill=False)
+# aspecto circular
+axes.set_aspect(1)
+axes.add_artist(Drawing_uncolored_circle)
+plt.title("Circle")
+
+plt.show()
